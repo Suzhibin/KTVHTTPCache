a0497fae5c65673a70313f725243151340339689
Andi <>
2023-11-08 18:33:05 +0800

    Update cachePath

--- KTVHTTPCache/Classes/KTVHCTools/KTVHCPathTool.m
+++ KTVHTTPCache/Classes/KTVHCTools/KTVHCPathTool.m
@@ -66,7 +66,8 @@
 
 + (NSString *)basePath
 {
-    return [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject];
+   // return [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject];
+    return [NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES) lastObject];
 }
 
 + (BOOL)isRelativePath:(NSString *)path
